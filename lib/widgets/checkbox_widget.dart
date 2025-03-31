94ec7c590c31c9defaeb14d94865199ea57de9bd
levon-altunyan <altunyan991@gmail.com>
2025-03-31 13:07:17 +0400

    Privacy Policy Google Analytics (custom events)

---
+++ lib/widgets/checkbox_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CheckboxWidget extends StatefulWidget {
+  const CheckboxWidget({super.key, required this.setValue});
+
+  final Function(bool value) setValue;
+
+  @override
+  State<CheckboxWidget> createState() => _CheckboxWidgetState();
+}
+
+class _CheckboxWidgetState extends State<CheckboxWidget> {
+  bool isChecked = false;
+
+  Color getColor(Set<WidgetState> states) {
+    const Set<WidgetState> interactiveStates = <WidgetState>{
+      WidgetState.pressed,
+      WidgetState.hovered,
+      WidgetState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Color(0xff4D81E7);
+    }
+    return Colors.white;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      checkColor: Color(0xff4D81E7),
+      fillColor: WidgetStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        isChecked = value!;
+        widget.setValue(isChecked);
+        setState(() {});
+      },
+    );
+  }
+}
